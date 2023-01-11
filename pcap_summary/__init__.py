5151606ba76fb6b2dc85c76cfb7449d7e37c7571
Heath Brown <heathd.brown@gmail.com>
2023-01-11 11:47:50 -0600

    doc(docstrings) Add module docstring

--- pcap_summary/__init__.py
+++ pcap_summary/__init__.py
@@ -1,8 +1,8 @@
 # SPDX-FileCopyrightText: 2022-present Heath Brown <heathd.brown@gmail.com>
 #
 # SPDX-License-Identifier: MIT
-# Module contains common wireshark filters and methods to apply those filters.
-# In addition to wrappers around common pyshark.FileCapture fucntions to simplify application of filters.
+"""Module contains common wireshark filters and methods to apply those filters.
+  In addition to wrappers around common pyshark.FileCapture fucntions to simplify application of filters."""
 
 import pyshark
 
